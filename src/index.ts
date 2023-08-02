6ed5a743001fdcb188b5f0c2622de47b18969ca4
Ted Shaffer <ted@brightsign.biz>
2023-08-02 13:02:24 -0700

    read autoplay and display it.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
+import * as fs from 'fs';
+
 import { ImageModeType } from '@brightsign/bscore';
 
 console.log('Hello big world!')
 console.log(ImageModeType.ScaleToFill);
 
+process.argv.forEach(function (val, index, array) {
+  console.log(index + ': ' + val);
+});
+
+const autoPlayFilePath = process.argv[2];
+console.log('autoplay file path: ' + autoPlayFilePath);
+
+const autoplayFile: Buffer = fs.readFileSync(autoPlayFilePath);
+console.log('autoplay file: ' + autoplayFile);
+
+const autoplay = JSON.parse(autoplayFile.toString());
+console.log('autoplay: ' + autoplay);
+
+// const autoplayProperties = Object.keys(autoplay);
+// for (const autoplayProperty of autoplayProperties) {
+//   console.log('autoplay property: ' + autoplayProperty);
+// }
+
+const autoplayStr = JSON.stringify(autoplay, null, 4); // (Optional) beautiful indented output.
+console.log(autoplayStr); // Logs output to dev tools console.
